b496b73296925fc83661f3a58814a8c374163dfb
brianborchert <brian@capitano.dk>
2023-11-02 14:18:10 +0100

    Chapter 3 implemented

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,35 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Aflevering1.Models;
-
-namespace Aflevering1.Controllers;
-
-public class HomeController : Controller
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+namespace PartyInvites.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses
+            .Where(r => r.WillAttend == true));
+        }
     }
 }
