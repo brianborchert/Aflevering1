a9bfa2d8f3e5b20bef42b96611fe884d995ab518
brianborchert <brian@capitano.dk>
2023-11-03 06:52:15 +0100

    Everything turned into English

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,13 +3,13 @@ namespace Aflevering1.Models
 {
 	public class Product
 	{
-		[Required(ErrorMessage = "Indtast produktnavn")]
+		[Required(ErrorMessage = "Enter product name")]
 		public string? Name { get; set; }
 		
-		[Required(ErrorMessage = "Indtast antal")]
+		[Required(ErrorMessage = "Enter amount")]
 		public int? Amount { get; set; }
 		
-		[Required(ErrorMessage = "Vælg område")]
+		[Required(ErrorMessage = "Select section")]
 		public string? Section { get; set; }
 	}
 }
